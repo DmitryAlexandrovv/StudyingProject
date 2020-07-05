a40f3dc195599bffa8fb7bd67a785d846ce4e933
Alexandrov <dmalexandrovv@gmail.com>
2020-07-05 20:35:05 +0300

    implemented adding menu cards from the database

--- src/js/main.js
+++ src/js/main.js
@@ -88,7 +88,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modal = document.querySelector('.modal'),
           modalOpenBtns = document.querySelectorAll('[data-modal]'),
-          modalCloseBtns = document.querySelectorAll('[data-close]');
+          modalCloseBtns = document.querySelectorAll('[data-close]'),
+          modalTimerId = setTimeout(modalOpen,15000);
 
     function modalClose() {
         modal.classList.remove('show');
@@ -100,6 +101,7 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.classList.remove('hide');
         modal.classList.add('show');
         document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
     }
 
     modalOpenBtns.forEach((element) => {
@@ -123,4 +125,76 @@ window.addEventListener('DOMContentLoaded', () => {
             modalClose();
         }
     });
+
+    function showModal() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            modalOpen();
+            window.removeEventListener('scroll', showModal);
+        }
+    }
+
+    window.addEventListener('scroll', showModal);
+
+    //Cards
+
+    class Card {
+        constructor(src, alt, title, description, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const item = document.createElement('div');
+
+            item.innerHTML = `<div class="menu__item">
+                                  <img src=${this.src} alt=${this.alt}>
+                                  <h3 class="menu__item-subtitle">${this.title}</h3>
+                                  <div class="menu__item-descr">${this.description}</div>
+                                  <div class="menu__item-divider"></div>
+                                  <div class="menu__item-price">
+                                      <div class="menu__item-cost">Цена:</div>
+                                      <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                  </div>
+                              </div>`;
+
+            this.parent.append(item);
+        }
+    }
+
+    new Card(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        "9",
+        ".menu .container"
+    ).render();
+
+    new Card(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        "15",
+        ".menu .container"
+    ).render();
+
+    new Card(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        "13",
+        ".menu .container"
+    ).render();
 });
